1bc55d43366cff037c1583d63a52dfeac9dd7f99
luisEfe80 <luismendez80@hotmail.com>
2021-06-25 22:13:18 -0400

    Ajustes de versión

--- src/config/global.js
+++ src/config/global.js
@@ -78,12 +78,6 @@ export default {
     ],
     subMenu: [
       {
-        nombreRuta: 'actividad',
-        icono: 'far fa-question-circle',
-        titulo: 'Actividad didáctica',
-        desarrolloContenidos: true,
-      },
-      {
         nombreRuta: 'glosario',
         icono: 'fas fa-sort-alpha-down',
         titulo: 'Glosario',
@@ -113,134 +107,91 @@ export default {
   referencias: [
     {
       referencia:
-        'Zlackman, C. (2012). 100 años de moda. Sistema de Bibliotecas SENA: ',
+        'Heim, B. (s. f.). Tecnologías de virtualización - Virtualización. Conceptos de Virtualización.',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/primo-explore/fulldisplay?docid=sena_aleph000060086&vid=SENA&search_scope=sena_completo&tab=sena_completo&lang=es_ES&context=L',
+        'https://sites.google.com/site/virutalizacionyha/tecnologias-de-virtualizacion',
     },
     {
-      referencia:
-        'Castellanos, O., Fumeque A., y Ramirez D. (2011). Análisis de tendencias: de la información hacia la innovación. Universidad Nacional de Colombia. Sistema de Bibliotecas SENA: ',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000007947',
-    },
-    {
-      referencia:
-        'Harpersbazaar. (2019). La ropa que eliges afecta a tu estado de ánimo y cerebro, según expertos. En arpersbazaar.mx.',
-    },
-    {
-      referencia:
-        'Lafuente M., Navarro J., y Navarro J. (2005). Ilustración de moda. Sistema de Bibliotecas SENA',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000025549',
-    },
-    {
-      referencia:
-        'Laver, J.,  Albizua, H., y Fortea, B. (1990). Breve historia del traje y la moda. Sistema de Bibliotecas SENA. ',
-    },
-    {
-      referencia:
-        'Navarro, J.,  y Lafuente, M. (2010). Ilustración de moda: dibujo plano. Sistema de Bibliotecas SENA:',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/q6j6k0/sena_aleph000023241',
-    },
-    {
-      referencia:
-        'Áciedad Americana de Ensayos y Materiales (ASTM). (2003). Textiles. Editorial Staff. [Book]. Sistema de Bibliotecas SENA.',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000051551',
-    },
-    {
-      referencia:
-        'Szkutnicka, B., Cuenca, C., Koyama, A. (2010). El dibujo técnico de moda paso a paso. Sistema de Bibliotecas SENA.',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000057728',
+      referencia: 'VMware. (s. f.). Virtualización. VMware.',
+      link: 'https://www.vmware.com/co/solutions/virtualization.html',
     },
   ],
   glosario: [
     {
-      termino: 'MoodBoard',
+      termino: 'Binarios',
       significado:
-        'su traducción literal es mapa de inspiración y aunque su uso está muy asociado al ámbito de la decoración de interiores o la moda, <br/>realmente se utiliza en entornos creativos muy diversos, desde el diseño de productos o servicios al audiovisual o el diseño gráfico.',
+        'tipo de archivos que contiene información requerida para la ejecución de ciertas aplicaciones.',
     },
     {
-      termino: 'Suftware',
+      termino: 'Data center',
       significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
+        'es un espacio donde se aloja un conjunto de equipos e infraestructura con el fin de brindar servicios a un grupo específico de usuarios.',
     },
     {
-      termino: 'Pirognóstico',
+      termino: 'Librerías',
       significado:
-        'ensayo sin componentes reactivos, quiere decir, reconocimiento de los componentes, mediante la aplicación adecuada del fuego.',
+        'conjunto de códigos construidos en un lenguaje de programación que sirve para ser reutilizado por otras aplicaciones.',
     },
     {
-      termino: 'Áectores',
-      significado:
-        'en software de diseño, son líneas que se crean y permiten, expandir, modificar, al tamaño que sea sin pixelarse porque se unen por coordenadas matemáticas del programa y no se generan por números fijos.',
+      termino: 'SDN',
+      significado: 'redes definidas por software.',
     },
     {
-      termino: 'Moda',
-      significado:
-        'hábito repetitivo que identifica a una persona, una especie de presión social, que debe consumir, utilizar o hacer.',
+      termino: 'VM',
+      significado: 'máquina virtual.',
     },
     {
-      termino: 'Diseño',
-      significado:
-        'actividad creativa orientada a soluciones idóneas a ciertas problemáticas.',
+      termino: 'VLAN',
+      significado: 'red de área local virtual.',
     },
     {
-      termino: 'Tipología',
-      significado:
-        'estudio de los tipos o modelos que se usan para clasificar en diversas ciencias o disciplinas.',
-    },
-    {
-      termino: 'Bidimensional',
-      significado:
-        'que se representa según su altura y su anchura y no su profundidad.',
-    },
-    {
-      termino: 'Interés Inferior del menor',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
-    },
-    {
-      termino: 'Interseccionalidad',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
-    },
-    {
-      termino: 'Interés superior del menor',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
+      termino: 'VPN',
+      significado: 'red privada virtual.',
     },
   ],
   complementario: [
     {
       texto:
-        'BIC., (2015). “2 minutos para entender el desarrollo sostenible – Spanish”',
+        'Generalidades de la virtualización: AlbertoLopez TECH TIPS. (2021). ¿Qué es la virtualización? ¿Para qué sirve la Virtualización? Tipos de virtualización de sistemas. YouTube. ',
       tipo: 'Video',
-      link: 'https://www.google.com/',
+      link: 'https://youtu.be/K1vPbQtOlSc',
     },
     {
       texto:
-        'Arnold, M. y Osorio, F., (1998). Introducción a los conceptos básicos de la Teoría general de sistemas. Cinta moebio 3: 40-49',
+        'Tipos de virtualización: AlbertoLopez TECH TIPS. (2021). Tipos de virtualización. 10 tipos de virtualización explicadas (de S.O., VDI, VPS, hardware, etc.). YouTube.',
       tipo: 'Página web',
-      descarga: '/downloads/prueba.pdf',
+      link: 'https://youtu.be/LMl2_W7cRME',
     },
     {
       texto:
-        'Balboa, C. y Dominguez, M. (2014). Economía circular como marco para el ecodiseño: el modelo ECO-3. Informador Técnico (Colombia) Volumen 78, No. 1. Pág. 52-90.',
-      tipo: 'Artículo científico',
-      link: 'https://www.google.com/',
-    },
-    {
-      texto: 'Economía de Rosquilla. (s.f.).',
+        'Herramientas de virtualización de servidores: VMware. (s.f.). Download VMware Workstation Pro. VMware. ',
       tipo: 'Página web',
-      descarga: '/downloads/prueba.pdf',
+      link: 'VMware. (s.f.). Download VMware Workstation Pro. VMware',
     },
     {
-      texto: '2 minutos para entender el desarrollo sostenible – Spanish',
-      tipo: 'Video',
-      link: 'https://www.google.com/',
+      texto:
+        'Herramientas de virtualización de servidores: VirtualBox. (s.f.). Download VirtualBox. VirtualBox',
+      tipo: 'Página web',
+      link: 'https://www.virtualbox.org/wiki/Downloads',
+    },
+    {
+      texto:
+        'Herramientas de virtualización de servidores: Docs Microsoft. (2019). Install Hyper-V on Windows 10. Docs Microsoft.',
+      tipo: 'Página web',
+      link:
+        'https://docs.microsoft.com/en-us/virtualization/hyper-v-on-windows/quick-start/enable-hyper-v',
+    },
+    {
+      texto:
+        'Herramientas de virtualización de servidores: QEMU. (s.f.). Download QEMU. QEMU. ',
+      tipo: 'Página web',
+      link: 'https://www.qemu.org/download/',
+    },
+    {
+      texto:
+        'Herramientas de virtualización de servidores: Parallels. (s.f.). Run Windows on Mac. Parallels. ',
+      tipo: 'Página web',
+      link: 'https://www.parallels.com',
     },
   ],
   creditos: {
